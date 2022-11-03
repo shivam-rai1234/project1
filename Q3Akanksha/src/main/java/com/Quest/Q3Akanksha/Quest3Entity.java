af8bbcae1f0e9547d91d11df251105be1746b2ad
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-11-03 13:35:46 +0530

    commit

--- Q3Akanksha/src/main/java/com/Quest/Q3Akanksha/Quest3Entity.java
+++ Q3Akanksha/src/main/java/com/Quest/Q3Akanksha/Quest3Entity.java
@@ -1,4 +1,4 @@
-package com.Quest.Q2Akanksha;
+package com.Quest.Q3Akanksha;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -6,16 +6,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
 @Entity
 @Table(name="employee")
-public class QuestionEmployeeEntity {
-	
+public class Quest3Entity {
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer emp_id;
 	private Integer emp_salary;
 	private String emp_name;
-	
 	
 	public Integer getEmp_id() {
 		return emp_id;
@@ -23,22 +23,17 @@ public class QuestionEmployeeEntity {
 	public void setEmp_id(Integer emp_id) {
 		this.emp_id = emp_id;
 	}
-	public String getEmp_name() {
-		return emp_name;
-	}
-	public void setEmp_name(String emp_name) {
-		this.emp_name = emp_name;
-	}
-	
-
 	public Integer getEmp_salary() {
 		return emp_salary;
 	}
 	public void setEmp_salary(Integer emp_salary) {
 		this.emp_salary = emp_salary;
 	}
-
-	
-	
+	public String getEmp_name() {
+		return emp_name;
+	}
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
 	
 }
