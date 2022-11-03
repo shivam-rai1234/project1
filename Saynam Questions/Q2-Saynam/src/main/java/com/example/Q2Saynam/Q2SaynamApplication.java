3d7e1cecd9d1362c48f11e265e9f0f8f0d2f5d55
Saynam221b <saynam1101@gmail.com>
2022-11-03 11:20:15 +0530

    commit by saynam sharma

---
+++ Saynam Questions/Q2-Saynam/src/main/java/com/example/Q2Saynam/Q2SaynamApplication.java
@@ -0,0 +1,17 @@
+package com.example.Q2Saynam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SaynamSharma
+ *
+ */
+@SpringBootApplication
+public class Q2SaynamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q2SaynamApplication.class, args);
+	}
+
+}
