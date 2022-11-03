64cae67de3a98b47438af9da167ec2b151efb9c2
shivam-rai1234 <shivam.rai@kpipartners.com>
2022-11-03 09:57:51 +0530

    commit for stand alone application by spring tool

---
+++ Shivam_rai/src/main/java/com/question1/Shivam_rai/ShivamRaiApplication.java
@@ -0,0 +1,13 @@
+package com.question1.Shivam_rai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivamRaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShivamRaiApplication.class, args);
+	}
+
+}
