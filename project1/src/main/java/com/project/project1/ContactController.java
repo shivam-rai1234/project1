ef10e821dd2bbcfdffc4eb4e9f3f230c61398a9c
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-11-03 20:28:24 +0530

    Commit for project 1

--- project1/src/main/java/com/project/project1/ContactController.java
+++ project1/src/main/java/com/project/project1/ContactController.java
@@ -1,5 +1,30 @@
 package com.project.project1;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContactController {
 
+	@Autowired
+	private ContactRepo cl;
+
+	@GetMapping("/contact")
+	public List<ContactEntity> getSample() {
+
+		return cl.getjoin1();
+	}
+	@GetMapping("/contact/emergency")
+	public List<ContactEntity> getSample1() {
+
+		return cl.getjoin2();
+	}
+	@GetMapping("/contact/CompanySupport")
+	public List<ContactEntity> getSample2() {
+
+		return cl.getjoin3();
+	}
 }
--- project1/src/main/java/com/project/project1/ContactRepo.java
+++ project1/src/main/java/com/project/project1/ContactRepo.java
@@ -8,11 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface ContactRepo extends JpaRepository<ContactEntity,Integer>
 {
 	
-
-	@Query(value = "Select * From contact where support=Emergency", nativeQuery = true)
+	@Query(value = "Select * From contact", nativeQuery = true)
 	public List<ContactEntity> getjoin1();
-
-	@Query(value = "Select * From client1 where support=Emergency", nativeQuery = true)
+	
+	@Query(value = "Select * From contact where support_id=112", nativeQuery = true)
 	public List<ContactEntity> getjoin2();
+
+	@Query(value = "Select * From contact where support_id=0", nativeQuery = true)
+	public List<ContactEntity> getjoin3();
 
 }
