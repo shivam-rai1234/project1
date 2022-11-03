8fc6f91a0d80af43161c697a0b1d92d20964faaf
Abhitayu <abhitayu.sharma@kpipartners.com>
2022-11-03 10:59:30 +0530

    Committing Employee Database from Abhitayu Sharma

---
+++ Q2EmpTable-Abhitayu/src/test/java/com/example/Q2EmpTableAbhitayu/Q2EmpTableAbhitayuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Q2EmpTableAbhitayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q2EmpTableAbhitayuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
