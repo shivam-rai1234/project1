75964ebeba36a62ee2afd4558087f6acdb958bb5
gauravborse4u <gauravborse4u@gmail.com>
2022-11-03 10:03:13 +0530

    commit by gaurav

---
+++ ProjectWithoutServer/src/main/java/com/example/simpleProject/ProjectWithoutServer/ProjectWithoutServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.simpleProject.ProjectWithoutServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//Gaurav Borse
+
+@SpringBootApplication
+public class ProjectWithoutServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithoutServerApplication.class, args);
+	}
+
+}
