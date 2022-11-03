40594b8e9135c1eeb286abc6403374c2367301e0
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-11-03 10:04:23 +0530

    //Akanksha Sinha

---
+++ Question1of3/src/test/java/com/Question/Question1of3/Question1of3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Question.Question1of3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Question1of3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
