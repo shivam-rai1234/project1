275063330d05a81e40ea48c772895adfa6f46a3d
AbhishekYadavKPI <abhishek.yadav@kpipartners.com>
2022-11-03 10:55:56 +0530

    Commit for "Question 2"

---
+++ Question2_Abhishek/src/main/java/com/KPI/Question2/Question2_Abhishek/Question2AbhishekApplication.java
@@ -0,0 +1,13 @@
+package com.KPI.Question2.Question2_Abhishek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Question2AbhishekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Question2AbhishekApplication.class, args);
+	}
+
+}
