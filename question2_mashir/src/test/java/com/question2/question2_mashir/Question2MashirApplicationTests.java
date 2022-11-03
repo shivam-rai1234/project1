9c88b128e34169452ee23a91e46c9316a365c47e
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-11-03 11:07:55 +0530

    Commit for Mashir Question 2

---
+++ question2_mashir/src/test/java/com/question2/question2_mashir/Question2MashirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.question2.question2_mashir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Question2MashirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
