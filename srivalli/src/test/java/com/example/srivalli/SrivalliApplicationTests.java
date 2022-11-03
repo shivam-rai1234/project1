7a24da86a35f16a7667a96a9b340dbd236abfd67
srivalli-bandaru <114054086+srivalli-bandaru@users.noreply.github.com>
2022-11-03 11:30:27 +0530

    commit for srivalli

---
+++ srivalli/src/test/java/com/example/srivalli/SrivalliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srivalli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrivalliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
