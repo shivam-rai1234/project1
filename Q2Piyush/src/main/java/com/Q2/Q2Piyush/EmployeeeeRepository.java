a824a5242ff72fe329e7ad655637734825457cf1
piyushbodhani1 <piyush.bodhani@kpipartners.com>
2022-11-03 11:16:53 +0530

    commit by "piyush for Q2"

---
+++ Q2Piyush/src/main/java/com/Q2/Q2Piyush/EmployeeeeRepository.java
@@ -0,0 +1,7 @@
+package com.Q2.Q2Piyush;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeeeRepository extends CrudRepository<EmployeeeeEntity, Integer> {
+
+}
