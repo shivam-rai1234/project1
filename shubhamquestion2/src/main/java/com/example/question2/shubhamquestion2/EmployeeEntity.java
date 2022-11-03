8b96c766521f49f7cbee750e34c0065471188f72
Shubham Khule <shubham.khule@kpipartners.com>
2022-11-03 11:27:59 -0700

    commit

---
+++ shubhamquestion2/src/main/java/com/example/question2/shubhamquestion2/EmployeeEntity.java
@@ -0,0 +1,45 @@
+package com.example.question2.shubhamquestion2;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+ 
+@Entity
+@Table(name="employee")
+
+public class EmployeeEntity{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String employee_name;
+	private String salary;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEmployee_name() {
+		return employee_name;
+	}
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+	
+	
+	
+}
+
---
+++ shubhamquestion2/src/main/java/com/example/question2/shubhamquestion2/EmployeeRepoJPA.java
@@ -0,0 +1,8 @@
+package com.example.question2.shubhamquestion2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepoJPA extends JpaRepository<EmployeeEntity,Integer>{
+
+}
