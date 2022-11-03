f95d31cebe55129944cc3424f816c756b50c201f
Anam-Tamboli <tamboli.anam@Kpipartners.com>
2022-11-03 11:55:10 +0530

    commit

---
+++ AnamTamboli/que2anam/src/main/java/com/anamtamboli/que2anam/EmployeeRepo.java
@@ -0,0 +1,7 @@
+package com.anamtamboli.que2anam;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepo extends CrudRepository<EmployeeEntity, Integer>  {
+
+}
---
+++ AnamTamboli/que2anam/src/main/java/com/anamtamboli/que2anam/Que2anamApplication.java
@@ -0,0 +1,13 @@
+package com.anamtamboli.que2anam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Que2anamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Que2anamApplication.class, args);
+	}
+
+}
