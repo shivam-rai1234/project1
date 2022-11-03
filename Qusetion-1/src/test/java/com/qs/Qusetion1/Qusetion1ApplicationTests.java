db27511cd9dcac09ee3377cb0a0b466d33810a61
Saynam221b <saynam1101@gmail.com>
2022-11-03 09:54:52 +0530

    Commit by Saynam Sharma

---
+++ Qusetion-1/src/test/java/com/qs/Qusetion1/Qusetion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qs.Qusetion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qusetion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
