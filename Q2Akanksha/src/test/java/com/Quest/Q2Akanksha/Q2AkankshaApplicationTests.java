1eb038e7670200e506a455b212f98b64ea5cbc4d
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-11-03 10:59:50 +0530

    Akanksha Sinha Question2

---
+++ Q2Akanksha/src/test/java/com/Quest/Q2Akanksha/Q2AkankshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Quest.Q2Akanksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q2AkankshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
