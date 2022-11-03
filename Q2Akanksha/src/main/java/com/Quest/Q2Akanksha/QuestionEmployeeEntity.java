1d412fdb3ddcad8fcf035513877c94bc71fa6904
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-11-03 11:38:45 +0530

    Commit Akanksha Sinha

--- Q2Akanksha/src/main/java/com/Quest/Q2Akanksha/QuestionEmployeeEntity.java
+++ Q2Akanksha/src/main/java/com/Quest/Q2Akanksha/QuestionEmployeeEntity.java
@@ -25,14 +25,16 @@ public class QuestionEmployeeEntity {
 	public void setEmp_name(String emp_name) {
 		this.emp_name = emp_name;
 	}
-	public String getEmp_salary() {
+	
+	private String emp_name;
+	public Integer getEmp_salary() {
 		return emp_salary;
 	}
-	public void setEmp_salary(String emp_salary) {
+	public void setEmp_salary(Integer emp_salary) {
 		this.emp_salary = emp_salary;
 	}
-	private String emp_name;
-	private String emp_salary;
+
+	private Integer emp_salary;
 	
 	
 }
