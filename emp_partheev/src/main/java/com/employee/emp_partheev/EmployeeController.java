943a077daf182784500dde92b2ae0238385ef026
partheev-boora <partheev.boora@kpipartners.com>
2022-11-03 11:37:46 +0530

    commit

--- emp_partheev/src/main/java/com/employee/emp_partheev/EmployeeController.java
+++ emp_partheev/src/main/java/com/employee/emp_partheev/EmployeeController.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepo er;
