6056bfb93720f5bf988240a10aa8744330ba2fb9
sakshaminde <sakshaminde3454@gmail.com>
2022-11-03 11:13:13 +0530

    Commit for Inde Saksham

---
+++ Q1Saksham/src/main/java/com/example/assignment3/EmployeeRepo.java
@@ -0,0 +1,8 @@
+package com.example.assignment3;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepo extends CrudRepository<EmployeeEntity, Integer>{
+	
+
+}
