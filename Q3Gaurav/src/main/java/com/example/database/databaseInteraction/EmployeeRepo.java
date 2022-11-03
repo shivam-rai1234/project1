fcf2b1d685afe8a8413d68e380d89881ab163332
gauravborse4u <gauravborse4u@gmail.com>
2022-11-03 16:30:46 +0530

    commit by gaurav

---
+++ Q3Gaurav/src/main/java/com/example/database/databaseInteraction/EmployeeRepo.java
@@ -0,0 +1,9 @@
+package com.example.database.databaseInteraction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<EmployeeEntity, Integer>{
+
+	
+	
+}
