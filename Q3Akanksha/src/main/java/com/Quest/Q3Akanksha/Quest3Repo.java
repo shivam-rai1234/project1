835b15a46c752107455441fbc8aa9796b37192a4
AkankshaSinha27 <114054003+AkankshaSinha27@users.noreply.github.com>
2022-11-03 15:01:54 +0530

    commit Akanksha

--- Q3Akanksha/src/main/java/com/Quest/Q3Akanksha/Quest3Repo.java
+++ Q3Akanksha/src/main/java/com/Quest/Q3Akanksha/Quest3Repo.java
@@ -1,7 +1,15 @@
 package com.Quest.Q3Akanksha;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface Quest3Repo extends CrudRepository<Quest3Entity,Integer>{
 
+	
+	@Query(value="select * from employee where emp_name like 'a%'",nativeQuery=true)
+	public List<Quest3Entity> getjoin(Integer a);
+	
+	
 }
