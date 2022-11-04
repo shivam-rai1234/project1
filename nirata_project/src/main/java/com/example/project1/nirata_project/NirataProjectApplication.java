89368b36f82f876a9a39aed748f4377a70c8332c
niratasinha <nirata.sinha@kpipartners.com>
2022-11-04 10:38:07 +0530

    commit

---
+++ nirata_project/src/main/java/com/example/project1/nirata_project/NirataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.project1.nirata_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirataProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirataProjectApplication.class, args);
+	}
+
+}
---
+++ nirata_project/src/test/java/com/example/project1/nirata_project/NirataProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project1.nirata_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NirataProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
