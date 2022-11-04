3bb39d9d061a743516b004b792aa317dcfaa2ba2
Anam-Tamboli <tamboli.anam@Kpipartners.com>
2022-11-04 10:32:27 +0530

    Commit For Metadata

---
+++ FinalProAnam/anamproject1/src/main/java/com/kpi/project1/anamproject1/Anamproject1Application.java
@@ -0,0 +1,13 @@
+package com.kpi.project1.anamproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anamproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Anamproject1Application.class, args);
+	}
+
+}
---
+++ FinalProAnam/anamproject1/src/test/java/com/kpi/project1/anamproject1/Anamproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.project1.anamproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anamproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
