3f20c224a30c75f6b55d40959118c53418c2811a
akshit9179 <akshit.anand@kpipartners.com>
2022-11-04 10:41:56 +0530

    commit

---
+++ akshit_project/src/main/java/ecommerce/akshit_project/AkshitProjectApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.akshit_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkshitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkshitProjectApplication.class, args);
+	}
+
+}
---
+++ akshit_project/src/test/java/ecommerce/akshit_project/AkshitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.akshit_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkshitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
