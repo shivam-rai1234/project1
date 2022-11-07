4efb75d8db7854ff3e2fbb7f7ea800f41e56504f
Om-Singh-1 <114053628+Om-Singh-1@users.noreply.github.com>
2022-11-07 10:11:14 +0530

    Updated css properties

--- ProductsHome/src/test/java/com/kpi/ProductsHomeApplicationTests.java
+++ ProductsHome/src/test/java/com/kpi/ProductsHomeApplicationTests.java
@@ -1,13 +1,31 @@
 package com.kpi;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kpi.entity.Items;
+import com.kpi.repository.ItemsRepository;
+import com.kpi.service.ItemsService;
 
 @SpringBootTest
 class ProductsHomeApplicationTests {
 
+	@Autowired
+	ItemsService service;
+	
+	Items item;
+	
 	@Test
 	void contextLoads() {
+		
+		String valueAtZero = "One Plus Nord CE 2 5G";
+		
+		assertEquals(15, service.getOnlyProductList().size());				// Not recommend usage just here for my testing purpose refer the below test function.
+		assertThat(service.getOnlyProductList().get(0)).isEqualTo(valueAtZero);
 	}
 
 }
